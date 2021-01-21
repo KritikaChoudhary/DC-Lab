adaa09af451157f68aeac02ab01202b67a93bb6a
KritikaChoudhary <kritikac2410@gmail.com>
2021-01-21 22:51:26 +0530

    Add files via upload

--- Part A/a1/a1.c
+++ Part A/a1/a1.c
@@ -1,81 +1,76 @@
+//Write parallel program using OpenMP to sort n element using merge sort.
+
 #include<stdio.h>
 #include<omp.h>
+#include<stdlib.h>
+#define n 6
 
-void merge(int a[], int low, int mid, int high)
+void merge(double a[], int low, int mid, int high)
 {
-    int j = low;
-    int k = mid;
-    int temp[50];
-    int i;
-    for(i = low; j < mid && k < high; i++)
-    {
-        if(a[j] < a[k])
-        {
-            temp[i] = a[j];
-            j++;
-        }
-        else
-        {
-            temp[i] = a[k];
-            k++;
-        }
-    }
-    if(j < mid)
-    {
-        for(;j < mid; j++)
-        {
-            temp[i++] = a[j];
-        }
-    }
-    else
-    {
-        for(;k < high; k++)
-        {
-            temp[i++] = a[k];
-        }
-    }
-    for(int i = 0; i < high; i++)
-    {
-        a[i] = temp[i];
-    }
+	double temp[high + 1];
+	int i,j,k;
+	
+	for(i = low, j = mid+1, k = low; i <=mid && j <=high; k++)
+	{
+		if(a[i] < a[j])
+			temp[k] = a[i++];
+		else
+			temp[k] = a[j++]; 
+	}
+	while(i <=mid)
+		temp[k++] = a[i++];
+	while(j <=high)
+		temp[k++] = a[j++];
+	
+	for(i = low; i<=high; i++)
+		a[i] = temp[i];
 }
 
-void mergesort(int a[], int low, int high)
+void mergesort(double a[], int low, int high)
 {
-    int mid;
-    if(low < high)
-    {
-        mid = (low + high) / 2;
-
-        #pragma omp parallel sections num_threads(2)        //creates 2 threads and iddentifies the code section that is to be divided among the threads
-        {
-            #pragma omp section         //distributes work among threads
-            {
-                mergesort(a, low, mid);
-            }
-            #pragma omp section
-            {
-                mergesort(a, mid + 1, high);
-            }
-        }
-        merge(a, low, mid, high);
-    }
+	int mid;
+	
+	if(low < high)
+	{
+		mid = (low + high) / 2;
+		
+		#pragma omp parallel sections num_threads(4)
+		{
+			#pragma omp section
+			{
+				mergesort(a, low, mid);
+			}
+			#pragma omp section
+			{
+				mergesort(a, mid+1, high);
+			}
+		
+		}
+		merge(a, low, mid, high);
+	}
 }
 
-int main()
+void main()
 {
-    int n;
-    printf("Array size:");
-    scanf("%d", &n);
-    printf("\nArray:");
-    int a[n];
-    for(int i = 0; i < n ; i++)
-        scanf("%d", &a[i]);
-    mergesort(a, 0, n - 1);
-    printf("Sorted array:");
-    for(int i = 0; i < n ; i++)
-        printf("%d\t", a[i]);
-
-    return 0;
+	double a[n]; 
+	
+	for(int i = 0; i  < n; i++)
+	{		
+		a[i] = rand() % 10;
+	}
+	
+	double start = omp_get_wtime();
+	mergesort(a, 0, n-1);
+	double timeElapsed = omp_get_wtime() - start;
+		
+	for(int j = 0; j<n; j++)
+		printf("%f\t",a[j]); 
+			
+	printf("Time: %f\n",timeElapsed);
+		
 }
+	
+ 
+
+
 
